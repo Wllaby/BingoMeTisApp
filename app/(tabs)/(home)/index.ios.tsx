5787c280f36840d97c8ef298e2bbf3a4c5e79ad2
natively.dev <api@natively.dev>
2026-01-25 23:33:12 +0000

    fix: Resolve missing default export error on iOS

--- app/(tabs)/(home)/index.ios.tsx
+++ app/(tabs)/(home)/index.ios.tsx
@@ -1,4 +1,6 @@
 
 // The iOS file is identical to the Android/Web version for this component
 // Import from the base file to avoid code duplication
-export { default } from './index';
+import HomeScreen from './index';
+
+export default HomeScreen;
